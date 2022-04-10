7f097510083fd581586546c13ca00341ebfb31e1
mkenihandev <mkenihan96@gmail.com>
2022-04-10 01:19:17 +0000

    Add code to increment correct and incorrect scores

--- assets/js/script.js
+++ assets/js/script.js
@@ -47,9 +47,11 @@ function checkAnswer() {
     let isCorrect = userAnswer === calculatedAnswer[0];
 
     if (isCorrect) {
-        alert('Hey! You got it right!')
+        alert('Hey! You got it right!');
+        incrementScore();
     } else {
-        alert(`Aww, you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`)
+        alert(`Aww, you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`);
+        incrementWrongAnswer();
     }
 
     runGame(calculatedAnswer[1]);
@@ -75,12 +77,23 @@ function calculateCorrectAnswer() {
 
 }
 
+/**
+ * Gets current score from the DOM and adds 1
+ */
 function incrementScore() {
+
+    let oldScore = parseInt(document.getElementById('score').innerText);
+    document.getElementById('score').innerText = ++oldScore;
 
 }
 
+/**
+ * Gets current score of wrong answers from the DOM and adds 1
+ */
 function incrementWrongAnswer() {
 
+    let oldScore = parseInt(document.getElementById('incorrect').innerText);
+    document.getElementById('incorrect').innerText = ++oldScore;
 }
 
 function displayAdditionQuestion(operand1, operand2) {
