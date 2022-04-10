1deea8cd8d306c38abfb1788cfc5510b41dbf786
mkenihandev <mkenihan96@gmail.com>
2022-04-10 00:14:12 +0000

    Add structure of game functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,35 @@
-console.log("Hello!")
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+    
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAdditionQuestion() {
+    
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+    
+}
+
+function displayDivisionQuestion() {
+    
+}
